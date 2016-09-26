00e1bc50452f97ac0dce0b49a2b2667662556976
Kostas Platis <platico.dev@gmail.com>
2016-09-27 01:32:10 +0200

    Added explanation comments

--- converter.php
+++ converter.php
@@ -3,25 +3,30 @@
 include 'simple_html_dom.php';
 
 /**
- * Created by PhpStorm.
  * User: platico
  * Date: 9/27/16
  * Time: 12:05 AM
  */
 
 
-get_bibtex_from('http://oswinds.csd.auth.gr/publications/','span','pub_bibtex','output.html');
-
+/**
+ * @param $url url of the page to be parsed
+ * @param $bibtex_wrapper the wrapper tag of the bibtex (ex. span)
+ * @param $bibtex_class the class of the wrapper tag
+ * @param $filename file name of the oupput file
+ */
 function get_bibtex_from($url,$bibtex_wrapper,$bibtex_class,$filename){
-//gets the source code of the html page
+    //gets the source code of the html page
     $html = file_get_html($url);
+
+    //appends first elements of the output html file
     $htmlFileData = "<html>\n<head></head>\n<body>\n";
     file_put_contents($filename, $htmlFileData, FILE_APPEND | LOCK_EX);
 
 
-//finds all the text wrapped by the specific wrapper in a specific class
+    //finds all the text wrapped by the specific wrapper in a specific class
     foreach($html->find($bibtex_wrapper.'.'.$bibtex_class) as $e){
-        file_put_contents($filename, $e->innertext.'</br>', FILE_APPEND | LOCK_EX);
+        file_put_contents($filename, $e->innertext.'</br>', FILE_APPEND | LOCK_EX); //prints the in the file
     }
 
     $htmlFileData = "\n</html>";
